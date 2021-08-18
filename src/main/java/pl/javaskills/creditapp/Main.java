5591a6fd088685fb504154b9081b2b2325e45c42
MichalWes <massive7m@gmail.com>
2021-08-18 14:22:24 +0200

    Modified for SLF4J no providers warning

--- src/main/java/pl/javaskills/creditapp/Main.java
+++ src/main/java/pl/javaskills/creditapp/Main.java
@@ -2,7 +2,7 @@ package pl.javaskills.creditapp;
 
 import pl.javaskills.creditapp.client.ConsoleReader;
 import pl.javaskills.creditapp.core.*;
-import pl.javaskills.creditapp.core.model.LoanApplication;
+import pl.javaskills.creditapp.core.model.*;
 import pl.javaskills.creditapp.core.PersonScoringCalculator;
 import pl.javaskills.creditapp.core.scoring.EducationCalculator;
 import pl.javaskills.creditapp.core.scoring.IncomeCalculator;
@@ -11,7 +11,15 @@ import pl.javaskills.creditapp.core.scoring.MaritalStatusCalculator;
 public class Main {
 
     public static void main(String[] args) {
-        LoanApplication loanApplication = new ConsoleReader().readInputParameters();
+        LoanApplication loanApplication = new LoanApplication(new Person(new PersonalData("Michal", "Wes", "Wis" ),new ContactData()), new PurposeOfLoan());
+        loanApplication.getPerson().getPersonalData().setMaritalStatus(MaritalStatus.MARRIED);
+        loanApplication.getPerson().getPersonalData().setEducation(Education.TERTIARY);
+        loanApplication.getPerson().getPersonalData().setNumOfFamilyDependants(1);
+        loanApplication.getPerson().getPersonalData().setTotalMonthlyIncomePLN(10000.0);
+        loanApplication.getPurposeOfLoan().setPeriod((byte)30);
+        loanApplication.getPurposeOfLoan().setType(Type.MORTGAGE);
+        loanApplication.getPurposeOfLoan().setAmount(300000);
+        //LoanApplication loanApplication = new ConsoleReader().readInputParameters();
         CreditApplicationDecision decision = new CreditApplicationService(new PersonScoringCalculator(new IncomeCalculator(), new EducationCalculator(), new MaritalStatusCalculator()), new CreditRatingCalculator()).getDecision(loanApplication);
         System.out.println(decision.getDecisionString());
     }
